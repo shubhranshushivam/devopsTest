b710971615c81b4cbbc575dde6b1ec9032eaa2f8
shubhranshushivam <52876165+shubhranshushivam@users.noreply.github.com>
2021-09-22 15:33:25 +0530

    Update devops.py

--- devops.py
+++ devops.py
@@ -1,15 +1,4 @@
-def prefix(s, words):
-    lis = []
-    for i in words:
-        if i.startswith(s):
-            lis.append(i)
-    return lis
-
-
-s = input("Enter a query string=")
-words = []
-x = int(input("enter size of list="))
-for i in range(x):
-    words.append(input('Enter a word='))
-
-print(prefix(s, words))
+print("HELLO WORLD")
+print("Hello from JENKINS")
+a=7
+print("hey this is",a)
